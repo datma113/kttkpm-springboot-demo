e2cfd2828d125b25ecf57f36033886df1c3d73bc
datma <datma113112111@gmail.com>
2021-09-14 14:19:28 +0700

    add entity and repository

---
+++ src/main/java/thanhdat/entity/Student.java
@@ -0,0 +1,47 @@
+package thanhdat.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String lastname;
+	private String firstname;
+	private String email;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Student() {
+		super();
+	}
+	
+	
+}
---
+++ src/main/java/thanhdat/repository/StudentRepository.java
@@ -0,0 +1,5 @@
+package thanhdat.repository;
+
+public class StudentRepository {
+
+}
