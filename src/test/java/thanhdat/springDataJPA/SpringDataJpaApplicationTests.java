4f65d18fdb50db5eb6fb60c3fb10069339d9623c
datma <datma113112111@gmail.com>
2021-09-14 14:51:36 +0700

    completed native-branch

--- src/test/java/thanhdat/springDataJPA/SpringDataJpaApplicationTests.java
+++ src/test/java/thanhdat/springDataJPA/SpringDataJpaApplicationTests.java
@@ -1,13 +1,38 @@
 package thanhdat.springDataJPA;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import thanhdat.entity.Student;
+import thanhdat.repository.StudentRepository;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@Test
+	void getAllStudents() {
+		List<Student> students = studentRepository.getAllStudents();
+		System.out.println(students);
+	}
+	
+	@Test
+	void updateStudentById() {
+		studentRepository.updateStudentById(1, "datma", "lala", "ddddssss@gmail.com");
+	}
+	
+	@Test
+	void deleteStudentById(int id) {
+		studentRepository.deleteStudentById(1);
 	}
 
 }
