33ae86b7efc27900a02b2abc1f35e470c9b6435e
datma <datma113112111@gmail.com>
2021-09-14 14:58:05 +0700

    competed jpql-branch

--- src/main/java/thanhdat/repository/StudentRepository.java
+++ src/main/java/thanhdat/repository/StudentRepository.java
@@ -1,11 +1,29 @@
 package thanhdat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import thanhdat.entity.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-
+	
+	@Query(value="select * from student")
+	List<Student> getAllStudents();
+	
+	
+	@Transactional
+	@Modifying
+	@Query(value="delete student where id = :id")
+	void deleteStudentById(int id);
+	
+	@Transactional
+	@Modifying
+	@Query(value="update student set lastname=:lastname, firstname=:firstname, email=:email  where id = :id ")
+	void updateStudentById(int id, String lastname, String firstname, String email);
 }
--- src/test/java/thanhdat/springDataJPA/SpringDataJpaApplicationTests.java
+++ src/test/java/thanhdat/springDataJPA/SpringDataJpaApplicationTests.java
@@ -1,7 +1,13 @@
 package thanhdat.springDataJPA;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import thanhdat.entity.Student;
+import thanhdat.repository.StudentRepository;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
@@ -10,4 +16,26 @@ class SpringDataJpaApplicationTests {
 	void contextLoads() {
 	}
 
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	
+	@Test
+	void getAllStudents() {
+		List<Student> students = studentRepository.getAllStudents();
+		System.out.println(students);
+	}
+	
+	
+	@Test
+	void updateStudentById() {
+		studentRepository.updateStudentById(1, "datma", "lala", "ddddssss@gmail.com");
+	}
+	
+	
+	@Test
+	void deleteStudentById(int id) {
+		studentRepository.deleteStudentById(1);
+	}
 }
